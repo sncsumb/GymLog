66563149de8a694f2bbb6f7a9c02c4c7d1fcf3f5
Serena Ngo <sngo@csumb.edu>
2025-07-27 16:47:17 -0700

    created database for users

---
+++ app/src/main/java/com/example/gymlog/database/UserDAO.java
@@ -0,0 +1,25 @@
+package com.example.gymlog.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.gymlog.database.entities.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user); //this will take zero or more user objects
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM " + GymLogDatabase.USER_TABLE + " ORDER BY username")
+    List<User> getAllUsers();
+
+    @Query(" DELETE from " + GymLogDatabase.USER_TABLE) void deleteAll();
+}
